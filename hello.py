6d9e4a4c3feccb81532b6da5a1eafefd048683b2
JQuinnie <jenn8866@gmail.com>
2018-06-25 20:50:28 -0600

    Add first exercise on Python

---
+++ hello.py
@@ -0,0 +1,13 @@
+msg = "Hello World "
+name = input('What is your name? ')
+print(msg + name)
+
+import math
+
+X = int(input('Enter number x: '))
+y = int(input('Enter number y: '))
+power = X**y
+log = int(math.log(X, 2))
+
+print('X**y = ' + str(power))
+print('log(x) = ' + str(log))
