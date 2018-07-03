2c9e879694ab0937d3b4e76d397e7e1733818379
JQuinnie <jenn8866@gmail.com>
2018-07-02 23:56:32 -0600

    Add conditionals

--- hello.py
+++ hello.py
@@ -1,19 +1,19 @@
 msg = "Hello to "
-name = input('What is your name? ')
+name = input("What is your name? ")
 print(msg + name)
 
 import math
 
-X = int(input('Enter number x: '))
-y = int(input('Enter number y: '))
-power = X**y
+X = int(input("Enter number x: "))
+y = int(input("Enter number y: "))
+power = X ** y
 log = int(math.log(X, 2))
 
-print('X**y = ' + str(power))
-print('log(x) = ' + str(log))
+print("X**y = " + str(power))
+print("log(x) = " + str(log))
 
 # some methods
-print('Length of your name:', len(name))
+print("Length of your name:", len(name))
 print(name.upper())
 
 # String templating
@@ -26,3 +26,18 @@ hamster = "hamster"
 is_it_true = ham in hamster
 print(is_it_true)
 print(ham in hamster)
+
+# bool example
+cat_lover = True
+has_dog = True
+print(has_dog and not cat_lover)
+
+# conditionals
+another_name = input("What is your name? ")
+if another_name == "Jenn":
+    print(another_name, "is learning Python")
+elif another_name == "Nina":
+    print(another_name, "is learning with fellow students! Me too!")
+else:
+    print("You should totally learn Python, {}!".format(another_name))
+print("Have a great day {}!".format(another_name))
