b5ff9b11d1e4cfdea735c7e2e6178b6315370cca
JQuinnie <jenn8866@gmail.com>
2018-07-02 19:34:13 -0600

    Update with methods, string templating and booleans

--- hello.py
+++ hello.py
@@ -11,3 +11,18 @@ log = int(math.log(X, 2))
 
 print('X**y = ' + str(power))
 print('log(x) = ' + str(log))
+
+# some methods
+print('Length of your name:', len(name))
+print(name.upper())
+
+# String templating
+subject_template = "Thanks for learning {} with us {}!"
+print(subject_template.format("Python", "Jenn"))
+
+# Is a string in something? Boolean return
+ham = "ham"
+hamster = "hamster"
+is_it_true = ham in hamster
+print(is_it_true)
+print(ham in hamster)
