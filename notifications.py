4e5494847add77eb3f770ec0f182bfb84c433a7f
JQuinnie <jenn8866@gmail.com>
2018-07-03 15:42:11 -0600

    Update with simple function

--- notifications.py
+++ notifications.py
@@ -1,5 +1,20 @@
 praise = "You are doing great"
 praise = praise.upper()
 number_of_characters = len(praise)
-result = praise + "!" * number_of_characters
+# double division is floor division, to get an integer no floats
+result = praise + "!" * (number_of_characters // 2)
 print(result)
+
+advice = "Don't forget to ask for help"
+
+# function creation
+def yell(text):
+    text = text.upper()
+    number_of_characters = len(text)
+    result = text + "!" * (number_of_characters // 4)
+    print(result)
+
+
+yell("You are doing great")
+yell(advice)
+yell("Don't Repeat Yourself. Keep things DRY")
