a31219e5f25c91f6b84bbf70fe492231bbcd8a7e
JQuinnie <jenn8866@gmail.com>
2018-07-03 23:16:53 -0600

    Add for loop

--- simple_pw_checker.py
+++ simple_pw_checker.py
@@ -16,3 +16,7 @@ request = input("What would you like?  ")
 while "please" not in request:
     request = input("You seem to be missing a magic word.  What would you like?  ")
 print("Here you go!")
+
+statement = "Enjoy!"
+for letter in statement:
+    print(letter.upper())
