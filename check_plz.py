586279cb016f5c34a7edcbf6b56464cabdc0495d
JQuinnie <jenn8866@gmail.com>
2018-07-03 22:58:43 -0600

    Raising errors

--- check_plz.py
+++ check_plz.py
@@ -2,6 +2,8 @@ import math
 
 
 def split_check(total, number_of_ppl):
+    if number_of_ppl <= 1:
+        raise ValueError("More than 1 person is required to split the check")
     return math.ceil(total / number_of_ppl)
 
 
@@ -9,8 +11,9 @@ def split_check(total, number_of_ppl):
 try:
     total_due = float(input("What is the check total? "))
     number_of_ppl = int(input("Between how many people? "))
-except ValueError:
-    print("Please provide a valid value...")
-else:
     amount_due = split_check(total_due, number_of_ppl)
+except ValueError as err:
+    print("Please provide a valid value...")
+    print("({})".format(err))
+else:
     print("Each person owes ${}".format(amount_due))
