a513c8669368b88a870e70fd362d44a586efa5d8
JQuinnie <jenn8866@gmail.com>
2018-07-03 22:54:54 -0600

    Coersion handling error, expecting exceptions

--- check_plz.py
+++ check_plz.py
@@ -5,9 +5,12 @@ def split_check(total, number_of_ppl):
     return math.ceil(total / number_of_ppl)
 
 
-total_due = float(input("What is the check total? "))
-number_of_ppl = int(input("Between how many people? "))
-
-amount_due = split_check(total_due, number_of_ppl)
-
-print("Each person owes ${}".format(amount_due))
+# put variables that might cause an error in 'try' and if error is 'ValueError' do thing else, do other thing
+try:
+    total_due = float(input("What is the check total? "))
+    number_of_ppl = int(input("Between how many people? "))
+except ValueError:
+    print("Please provide a valid value...")
+else:
+    amount_due = split_check(total_due, number_of_ppl)
+    print("Each person owes ${}".format(amount_due))
