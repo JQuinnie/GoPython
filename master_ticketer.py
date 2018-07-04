e5cd80722dc97b5d9b0deab3258abfd00ecbc34b
JQuinnie <jenn8866@gmail.com>
2018-07-03 23:46:20 -0600

    Update with confirmation to proceed

--- master_ticketer.py
+++ master_ticketer.py
@@ -17,3 +17,17 @@ total_price = number_of_tickets * TICKET_PRICE
 
 # Out put the price to the screen
 print("Your total price is: {}".format(total_price))
+
+# Prompt user if they want to proceed. Y/N
+proceed = input("Would you like to proceed with the purchase order? (Y/N) ")
+
+# If they want to proceed
+if proceed.lower() == "y":
+    # print out to the screen 'SOLD!' to confirm purchase
+    print("Thank you for your purchase!")
+    # and then decrement the tickets remaining by the number of tickets purchased
+    tickets_remaining -= number_of_tickets
+# Otherwise...
+else:
+    # Thank them by name
+    print("{}, thank you for browsing!".format(name))
