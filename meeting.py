2d8726bbfd2272cd32c9f2f94b50d775d8048b67
JQuinnie <jenn8866@gmail.com>
2018-07-20 17:41:20 -0600

    Update with indexing examples

--- meeting.py
+++ meeting.py
@@ -2,7 +2,7 @@
 
 attendees = ["Ken", "Alena", "Treasure"]
 
-print("There are", len(attendees), "attendees to begin with.")
+print("There were", len(attendees), "attendees to begin with.")
 
 # append an item
 attendees.append("Craig")
@@ -18,3 +18,19 @@ more_attendees = ["Guil", "Ryan", "Dave"]
 all_attendees = attendees + more_attendees
 
 print("But now, there are", len(all_attendees), "attendees.")
+
+
+# books for the meeting
+books = [
+    "Automate the Boring Stuff with Python: Practical Programming for Total Beginners - Al Sweigart",
+    "Python for Data Analysis",
+    "Fluent Python: Clear, Concise, and Effective Programming - Luciano Ramalho",
+    "Python for Kids: A Playful Introduction To Programming - Jason R. Briggs",
+    "Hello Web App: Learn How to Build a Web App - Tracy Osborn",
+]
+
+print("Suggested reading: {}".format(books[0]))
+
+books.insert(0, "Learning Python: Powerful Object-Oriented Programming")
+# add string to 0 index, lists are mutable but strings are not, you can access a specific character on a String using an index, but you cannot change it
+books[0] += " - Mark Lutz"
