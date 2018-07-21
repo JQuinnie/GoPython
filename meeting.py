d217b7c81020e2b865d16ea3ef6d9e6b717d806f
JQuinnie <jenn8866@gmail.com>
2018-07-21 16:19:26 -0600

    Add additional for loop examples

--- meeting.py
+++ meeting.py
@@ -101,3 +101,23 @@ for week in travel_expenses:
     week_number += 1
 print("Total Expense: ${}".format(sum(sum(x) for x in travel_expenses)))
 
+
+# Here is a multi-dimensional list of musical groups. The first dimension is group, the second is group members.
+# Can you loop through each group and output the members joined together with a ", " comma space as a separator, please?
+musical_groups = [
+    ["Ad Rock", "MCA", "Mike D."],
+    ["John Lennon", "Paul McCartney", "Ringo Starr", "George Harrison"],
+    ["Salt", "Peppa", "Spinderella"],
+    ["Rivers Cuomo", "Patrick Wilson", "Brian Bell", "Scott Shriner"],
+    ["Chuck D.", "Flavor Flav", "Professor Griff", "Khari Winn", "DJ Lord"],
+    ["Axl Rose", "Slash", "Duff McKagan", "Steven Adler"],
+    ["Run", "DMC", "Jam Master Jay"],
+]
+# Your code here
+for group in musical_groups:
+    print(", ".join(group))
+
+# print only trios
+for group in musical_groups:
+    if len(group) == 3:
+        print(", ".join(group))
