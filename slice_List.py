a8eb55b4e32a850b520537516600462b495a6bc3
JQuinnie <jenn8866@gmail.com>
2018-07-23 17:18:39 -0600

    Update with more slicing

--- slice_List.py
+++ slice_List.py
@@ -8,3 +8,7 @@ print(messy_list, clean_list)
 
 # slice [start:finish], starting with and finish before
 print(clean_list[2:4])
+
+# return last two items in clean_list as last_list
+last_list = clean_list[len(clean_list) - 2 :]
+print(last_list)
