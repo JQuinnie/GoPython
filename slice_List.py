c6792c3f744754dc93dad6fc0b9352972907fc7a
JQuinn MacBook Air <jenn8866@gmail.com>
2018-07-23 22:45:28 -0600

    Add more slice features

--- slice_List.py
+++ slice_List.py
@@ -10,5 +10,18 @@ print(messy_list, clean_list)
 print(clean_list[2:4])
 
 # return last two items in clean_list as last_list
-last_list = clean_list[len(clean_list) - 2 :]
+last_list = clean_list[len(clean_list) - 2:]
 print(last_list)
+
+# get a range of numbers in list form and step every other
+number_list = list(range(20))
+even_number_list = number_list[::2]
+step_back = number_list[-2:-5:-1]
+reverse_list = number_list[::-1]
+last_4 = number_list[-4::1]
+
+print(number_list)
+print(even_number_list)
+print(step_back)
+print(reverse_list)
+print(last_4)
