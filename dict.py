277c134231d07b1425eff177879cdf33ceb19ab6
JQuinnie <jenn8866@gmail.com>
2018-07-24 11:33:32 -0600

    Update with dict pack/unpack

--- dict.py
+++ dict.py
@@ -10,3 +10,27 @@ del jenn["status"]
 jenn["status"] = "Hard worker"
 
 print(jenn)
+
+
+# most time dict packing/unpacking is in relations to functions, as that is when its most useful
+# **kwargs -> function will pack key word arguments
+def packer(**kwargs):
+    print(kwargs)
+
+
+# will not pack job key and value
+def packerJob(job=None, **kwargs):
+    print(kwargs)
+
+
+def unpacker(first_name=None, last_name=None):
+    if first_name and last_name:
+        print("Hi {} {}!".format(first_name, last_name))
+    else:
+        print("Hi no name!")
+
+
+packer(name="Jenn", exp=1, job=None)
+packerJob(name="Jenn", exp=1, job=None)
+unpacker(**{"last_name": "Chu", "first_name": "Jenn"})
+
