405d21cde49ff6bf660c71a22cfed7c3c6f016b5
JQuinnie <jenn8866@gmail.com>
2018-07-26 14:17:00 -0600

    Add packing into tuples functions

--- tuples.py
+++ tuples.py
@@ -10,3 +10,33 @@ a, b = b, a
 c = b, a
 print(a, b)
 print(c)
+
+# packing arbitrary numbers into functions
+def add(*nums):
+    total = 0
+    for num in nums:
+        total += num
+    return total
+
+
+print(add(5, 5))
+
+
+def add2(base, *args):
+    total = base
+    for num in args:
+        total += num
+    return total
+
+
+print(add2(5, 20))
+
+
+def multiply(*nums):
+    total = 1
+    for num in nums:
+        total *= num
+    return total
+
+
+print(multiply(2, 2, 2))
