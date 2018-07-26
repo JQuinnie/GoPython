a02a065ecea9d8de3796781d1db234d44c3b3371
JQuinnie <jenn8866@gmail.com>
2018-07-26 14:09:16 -0600

    pretty cool, no need for variable temp for swapping

--- tuples.py
+++ tuples.py
@@ -3,3 +3,10 @@ my_tuple = (1, 2, 3)
 # see what you can do with tuples
 dir(my_tuple)
 
+# advantage is simultaneous assignment, swapping without the need for a temp!
+a = "b"
+b = "a"
+a, b = b, a
+c = b, a
+print(a, b)
+print(c)
