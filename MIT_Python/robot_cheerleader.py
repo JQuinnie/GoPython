8e0ee9266976628b7f4cd67e472909de2bf100d0
JQuinn MacBook Air <jenn8866@gmail.com>
2018-07-28 17:17:22 -0600

    Refactor while loop out

--- MIT_Python/robot_cheerleader.py
+++ MIT_Python/robot_cheerleader.py
@@ -4,15 +4,16 @@ an_letters = "AEFHILMNORSX"
 word = input("I will cheer for you! Enter a word: ").upper()
 times = int(input("Enthusiastic level (0-10): "))
 
-i = 0
+# i = 0
 
-while i < len(word):
-    char = word[i]
+# while i < len(word):
+# char = word[i]
+for char in word:
     if char in an_letters:
         print("Give me an {}! {}".format(char, char))
     else:
         print("Give me a {}! {}".format(char, char))
-    i += 1
+    # i += 1
 
 print("What does that spell??")
 
