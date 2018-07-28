faa8bafa39e4b251a86c92b5a43266046ed3a1c0
JQuinn MacBook Air <jenn8866@gmail.com>
2018-07-28 17:14:59 -0600

    Add cute cheer

---
+++ MIT_Python/robot_cheerleader.py
@@ -0,0 +1,22 @@
+# let the robot cheerleader cheer for you!
+
+an_letters = "AEFHILMNORSX"
+word = input("I will cheer for you! Enter a word: ").upper()
+times = int(input("Enthusiastic level (0-10): "))
+
+i = 0
+
+while i < len(word):
+    char = word[i]
+    if char in an_letters:
+        print("Give me an {}! {}".format(char, char))
+    else:
+        print("Give me a {}! {}".format(char, char))
+    i += 1
+
+print("What does that spell??")
+
+for i in range(times):
+    print(word, "!!!")
+
+# print((word + "!!!\n") * (times))
