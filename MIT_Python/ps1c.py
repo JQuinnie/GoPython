95a47ccc8d27fac72ef1eb2d0275de00f90c3b41
JQuinnie <jenn8866@gmail.com>
2018-07-29 16:37:35 -0600

    Set up assignment

--- MIT_Python/ps1c.py
+++ MIT_Python/ps1c.py
@@ -1,2 +1,14 @@
-# Write a program to calculate the best savings rate, as a function of your starting salary.
+# Write a program to calculate the best savings rate, as a function of your starting salary, can you afford the down payment in 3 years time?
 
+
+def calc():
+    # annual salary
+    annual_salary = int("Enter the starting salary: ")
+    # semi-annual raise
+    semi_annual_raise = .07
+    # monthly return on investment rate
+    r = 0.04 / 12
+    # cost of the dream house
+    total_cost = 1000000
+    # down payment for dream house
+    down_payment = .25 * total_cost
